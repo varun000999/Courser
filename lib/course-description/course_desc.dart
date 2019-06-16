0f59ba7dabf9ac1cd396d36768692ea2f22c3647
Avnish <av_nish@outlook.com>
2019-06-16 14:20:25 +0530

    finished course page without reviews

--- lib/course-description/course_desc.dart
+++ lib/course-description/course_desc.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CourseDesc extends StatelessWidget{
   
-  var para = 'Lorem ipsum dolor sit amet';
+  var para = 'Lorem ipsum dolor sit amet kdjflakjdf ladsk fdskj fakjs dflakj sdfla lfj adlf aldf aldjf aldsjf aldsj faldsj faljs dfjlads fjlfad sflj adslfj djlf adsjlf adl faljf adlj fajldf adsljf aldsj fajlds ';
 
   @override 
   Widget build(BuildContext context){
@@ -43,6 +43,101 @@ class CourseDesc extends StatelessWidget{
       ),
     );
   
+  // Type of Course
+  
+  final typeText = Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        child: Text(
+          "Type",
+           style: TextStyle(color: Colors.black, fontFamily: "Roboto", fontSize: 18.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+
+  final typeTextValue = Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        child: Text(
+          "Machine Learning",
+          style: TextStyle(color: Colors.black, fontFamily: "Roboto", fontSize: 18.0, fontWeight: FontWeight.w400),
+        ),
+      ),
+    );  
+  
+  final courseType = Row(children: <Widget>[
+    typeText,
+    SizedBox(width: 145.0,),
+    typeTextValue
+  ],);
+
+  // Price of course
+  final priceText = Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        child: Text(
+          "Price",
+          style: TextStyle(color: Colors.black, fontFamily: "Roboto", fontSize: 18.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  
+  final priceTextValue = Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        child: Text(
+          "Free",
+          style: TextStyle(color: Colors.black, fontFamily: "Roboto", fontSize: 18.0, fontWeight: FontWeight.w400),
+        ),
+      ),
+    );  
+  
+  final coursePrice = Row(children: <Widget>[
+    priceText,
+    SizedBox(width: 145.0,),
+    priceTextValue
+  ],);
+  
+   final linkButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(10.0),
+      color: Colors.white,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("LINK",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 21.0,color: Colors.deepPurple)),
+      ),
+    );
+
+  final likeButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(10.0),
+      color: Colors.white,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Icon(Icons.thumb_up, color: Colors.grey,)
+      ),
+    );
+
+  final linkAndUpvote = Row(children: <Widget>[
+    linkButton, SizedBox(width: 100.0,),likeButton
+
+  ],);
+
+  // Reviews
+  final reviewText = Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        child: Text(
+          "Reviews",
+          style: TextStyle(color: Colors.black, fontFamily: "Roboto", fontSize: 18.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+
   final spacerCourseDesc = SizedBox(height: 20.0,);
 
   return Scaffold(
@@ -53,7 +148,15 @@ class CourseDesc extends StatelessWidget{
     courseHeading,
     spacerCourseDesc,
     courseDescText,
-    courseDescPara
+    courseDescPara,
+    spacerCourseDesc,
+    courseType, 
+    spacerCourseDesc,
+    coursePrice,
+    spacerCourseDesc,
+    linkAndUpvote,
+    spacerCourseDesc,
+    reviewText
   ],),
   ));
   }
