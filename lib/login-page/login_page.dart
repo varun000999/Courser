8ec61e9dbbc9ff39a625bf1289e97d3a896ccfe4
avnish98 <av_nish@outlook.com>
2019-06-20 10:51:31 -0400

    added basic UI functions to login page

--- lib/login-page/login_page.dart
+++ lib/login-page/login_page.dart
@@ -19,7 +19,7 @@ class _LoginPageState extends State<LoginPage> {
     final LButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(10.0),
-      color: Colors.purple,
+      color: Colors.deepPurple,
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
@@ -73,7 +73,7 @@ class _LoginPageState extends State<LoginPage> {
         child: Text(
           "Welcome to Courser",
           style: TextStyle(
-              color: Colors.purple, fontFamily: "Roboto", fontSize: 21.0),
+              color: Colors.deepPurple, fontFamily: "Roboto", fontSize: 21.0),
         ),
       ),
     );
