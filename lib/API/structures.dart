c39ece0dcde8c5aff9a9884783dbde7c863d3fe1
AMIT317 <threenamit@gmail.com>
2019-06-20 13:46:40 -1200

    added comment for course class

--- lib/API/structures.dart
+++ lib/API/structures.dart
@@ -1,14 +1,19 @@
 class Course {
+  /* stores data of course
+
+     purpose: to store and retrieve data related to course
+     */
+
   int cid; // Unique id of courses
-  String cname;
-  String uname;
-  String createdDate;
-  String desc;
-  String type;
-  String link;
-  String platform;
-  int _upvCount;
-  int free;
+  String cname; // course name
+  String uname; //user name
+  String createdDate; //date of creation of course
+  String desc; //description
+  String type; //type of course
+  String link; //link of course
+  String platform; //platform providing course(like udemy,coursera)
+  int _upvCount; //no. of upvotes/likes on course
+  int free; //represent boolean value free=1 and paid=0
 
   Course(int cid, String cname, String uname, String createdDate, String desc, 
         String type, String link, String platform, int upvCount, int free){
