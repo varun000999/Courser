16bc54238f99b6422961304c5e6555b3d99b8e32
AMIT317 <threenamit@gmail.com>
2019-06-20 14:06:55 -1200

    added comment for course class 2

--- lib/API/structures.dart
+++ lib/API/structures.dart
@@ -4,29 +4,30 @@ class Course {
      purpose: to store and retrieve data related to course
      */
 
-  int cid; // Unique id of courses
-  String cname; // course name
-  String uname; //user name
-  String createdDate; //date of creation of course
-  String desc; //description
-  String type; //type of course
-  String link; //link of course
-  String platform; //platform providing course(like udemy,coursera)
-  int _upvCount; //no. of upvotes/likes on course
-  int free; //represent boolean value free=1 and paid=0
+  int cid;  // Unique id of courses
+  String cname;  // course name
+  String uname;  //user name
+  String createdDate;  //date of creation of course
+  String desc;  //description
+  String type;  //type of course
+  String link;  //link of course
+  String platform;  //platform providing course(like udemy,coursera)
+  int _upvCount;  //no. of upvotes/likes on course
+  int free;  //represent boolean value free=1 and paid=0
 
-  Course(int cid, String cname, String uname, String createdDate, String desc, 
+  //constructor of course
+  Course(int cid, String cname, String uname, String createdDate, String desc,
         String type, String link, String platform, int upvCount, int free){
-    this.cid =  cid;
-    this.cname =  cname;
-    this.uname =  uname;
-    this.createdDate = createdDate;
-    this.desc =  desc;
-    this.type =  type;
-    this.link =  link;
-    this.platform =  platform;
-    this._upvCount =  _upvCount;
-    this.free =  free;     
+    this.cid =  cid;  //course id
+    this.cname =  cname;   //course name
+    this.uname =  uname;   //user name
+    this.createdDate = createdDate;   //date of creation of course
+    this.desc =  desc;   //description
+    this.type =  type;   //type of course
+    this.link =  link;   //link of course
+    this.platform =  platform;  //platform providing course(like udemy,coursera)
+    this._upvCount =  _upvCount;  //no. of upvotes/likes on course
+    this.free =  free;  //
   }
 
   int getUpv(){
